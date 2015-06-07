64010620bfa51e08ed4fa3541410ba603a8c89a4
James White <jamesmacwhite@users.noreply.github.com>
2015-06-07 09:10:02 +0100

    Consistency when written as the product name

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>ipmagnet</title>
+		<title>ipMagnet</title>
 		<link rel="icon" href="static/favicon.ico" type="image/x-icon" />
 		<link rel="stylesheet" type="text/css" href="static/ipmagnet.css" />
 		<script type="text/javascript" src="static/ajax.js"></script>
